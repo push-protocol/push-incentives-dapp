449c9e40757339f4f4603da074bcd237d9c4e9d6
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-04-17 19:57:26 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f29600d382e2bc5d8d95e1bb9ceafbe8.js"
+  "/precache-manifest.75dc9bdb40d1a335ad9412d2451fb9d2.js"
 );
 
 self.addEventListener('message', (event) => {

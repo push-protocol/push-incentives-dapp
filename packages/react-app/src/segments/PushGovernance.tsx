62408b3feee55e093c5fcbfc524419871b87b575
rijusougata13 <rijusougata13@gmail.com>
2022-03-02 18:06:31 +0530

    fix gasless not working

--- packages/react-app/src/segments/PushGovernance.tsx
+++ packages/react-app/src/segments/PushGovernance.tsx
@@ -230,20 +230,20 @@ function Delegate({ epnsReadProvider, epnsWriteProvide }) {
     }
     console.log("balance",tokenBalance);
    
-    if (tokenBalance == 0) {
-      toast.dark("No PUSH to Delegate!", {
-        position: "bottom-right",
-        type: toast.TYPE.ERROR,
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-      setTxInProgress(false);
-      return;
-    }
+    // if (tokenBalance == 0) {
+    //   toast.dark("No PUSH to Delegate!", {
+    //     position: "bottom-right",
+    //     type: toast.TYPE.ERROR,
+    //     autoClose: 5000,
+    //     hideProgressBar: false,
+    //     closeOnClick: true,
+    //     pauseOnHover: true,
+    //     draggable: true,
+    //     progress: undefined,
+    //   });
+    //   setTxInProgress(false);
+    //   return;
+    // }
     
     if(transactionMode === 'withgas'){
       executeDelegateTx(newDelegatee,epnsToken,toast,setTxInProgress,library,LoaderToast)
@@ -420,7 +420,7 @@ function Delegate({ epnsReadProvider, epnsWriteProvide }) {
                     <ItemH>
                     <RadioGroup >
                     <div>
-                    <input type="radio" id="gasless" checked   name="gasless" value="gasless" onChange={e=>setTransactionMode(e.target.value)}/> <br/>
+                    <input type="radio" id="gasless"    name="gasless" value="gasless" onChange={e=>setTransactionMode(e.target.value)}/> <br/>
                     <Label>Gasless </Label><br/>
                     </div>
                     <div>
